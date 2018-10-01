817538dde81fbccec675ed2281d96d240af544d7
Camille Brugel <camille.brugel@laposte.net>
2018-10-01 11:45:01 +0200

    Improve futures polling

--- src/cosche/scheduler.cpp
+++ src/cosche/scheduler.cpp
@@ -69,45 +69,54 @@ void Scheduler::attachBatch(const std::vector<TaskNode*>& dependees)
 
 void Scheduler::run()
 {
-    while (!_taskGraph.empty() || hasFutures())
+    do
     {
-        if (_taskGraph.empty())
-        {
-            std::this_thread::sleep_for(_futuresPollDelay);
-            continue;
-        }
+        checkFutures();
 
         _me = &(_taskGraph.top());
 
         contextSwitch(&(Scheduler::_coroutine),
                       &(_me->value->_coroutine));
     }
+     while (!_taskGraph.empty());
 
     _me = nullptr;
+
+    // TODO: deals with cyclic task graph here.
 }
 
-bool Scheduler::hasFutures()
+void Scheduler::checkFutures()
 {
-    bool returnValue = !_futuresTaskPairs.empty();
-
-    for (auto&& futureTaskPair : _futuresTaskPairs)
+    if (_futuresTaskPairs.empty())
     {
-        const auto& [futurePtr, taskNodePtr] = futureTaskPair;
-
-        auto&& taskNode = *taskNodePtr;
-
-        if (futurePtr->ready())
-        {
-            detach(taskNode,
-                   taskNode);
-
-            futureTaskPair = _futuresTaskPairs.back();
-
-            _futuresTaskPairs.pop_back();
-        }
+        return;
     }
 
-    return returnValue;
+    pollFutures:
+    {
+        for (auto&& futureTaskPair : _futuresTaskPairs)
+        {
+            const auto& [futurePtr, taskNodePtr] = futureTaskPair;
+
+            auto&& taskNode = *taskNodePtr;
+
+            if (futurePtr->ready())
+            {
+                detach(taskNode,
+                       taskNode);
+
+                futureTaskPair = _futuresTaskPairs.back();
+
+                _futuresTaskPairs.pop_back();
+            }
+        }
+
+        if (_taskGraph.empty())
+        {
+            std::this_thread::sleep_for(_futuresPollDelay);
+            goto pollFutures;
+        }
+    }
 }
 
 void Scheduler::releaseContext(TaskNode& taskNode)
